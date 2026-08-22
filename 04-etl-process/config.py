76a3b97ba2fb0d98a9526aa083575da6faef185d
supittra <supittra.p@kkumail.com>
2026-08-22 16:18:53 +0700

    Change source DB path to new data directory
    Updated the source database path to reflect new directory structure.

--- 04-etl-process/config.py
+++ 04-etl-process/config.py
@@ -2,7 +2,7 @@ import os
 
 # กำหนด Path ของฐานข้อมูลต้นทาง (SQLite) และปลายทาง (เช่น Data Warehouse หรือ SQLite DW)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SOURCE_DB_PATH = os.path.join(BASE_DIR, "01-operational-db", "sqlite-sakila.db")
+SOURCE_DB_PATH = os.path.join(BASE_DIR, "data", "sqlite-sakila.db")
 TARGET_DB_PATH = os.path.join(BASE_DIR, "05-data-warehouse", "sakila_dw.db")
 
 SOURCE_URI = f"sqlite:///{SOURCE_DB_PATH}"
